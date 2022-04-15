33eed9f28d96c4a5fafd8c5fe84b71d3a3d66199
CedricRabarijohn <95499695+CedricRabarijohn@users.noreply.github.com>
2022-04-15 11:12:13 +0300

    Commit

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ server.on('request',(req, res) => {
     res.writeHead(200)
     let url = new URL(req.url, `http://${req.headers.host}`)
     let name = url.searchParams.get('name')
-    fs.readFile('index.html', (err,data) => {
+    fs.readFile('index.html','utf8', (err,data) => {
         if(err){
             res.writeHead(404,{'Content-type':'text/html; charset=utf-8'})
-            res.end()
+            res.end("Une erreur s'est produite , veuillez reessayer plus tard")
         }else{
-
+            res.writeHead(200, {'Content-type':'text/html; charset=utf-8'})
+            let url = new URL(req.url, `http://${req.headers.host}`)
+            let name = url.searchParams.get('name') === null | url.searchParams.get('name') === '' ? 'anonyme' : url.searchParams.get('name')
+            data = data.replace('{{ name }}', name)
+            res.end(data)
         }
     })
 })
